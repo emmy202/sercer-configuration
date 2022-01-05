87675d7be116f49bfebb218aaa3470b277633160
emmy202 <hitimanaemm10@gmail>
2022-01-05 08:33:05 +0200

    Initial commit

---
+++ sercer-configuration/src/test/java/com/aos/sercerconfiguration/SercerConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aos.sercerconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SercerConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
